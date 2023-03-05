f543f712a06f9b38241bb7cf18a12b61f319fd2b
unknown <sabbirshuvo006@gmail.com>
2023-03-05 23:48:32 +0600

    plugin added

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <button class="btn btn-link">Button</button>
+      </div>
     </div>
   );
 }
