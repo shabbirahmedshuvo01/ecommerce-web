19970df50e5d8af8ba05d2513fece91ae80da7b1
unknown <sabbirshuvo006@gmail.com>
2023-03-12 00:21:50 +0600

    added router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import './App.css';
 import Home from './componentes/Home/Home';
 import Navbar from './componentes/Navbar/Navbar';
 import Footer from './componentes/Footer/Footer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './componentes/Autherntication/Login/Login';
+import Singup from './componentes/Autherntication/Singup/Singup';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>
+    },
+
+    {
+      path: "singup",
+      element: <Singup />
+    }
+
+  ])
+
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <RouterProvider router={router}></RouterProvider>
+      {/* <Home /> */}
       <Footer />
     </div>
   );
